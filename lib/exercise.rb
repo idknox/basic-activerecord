a7bdf29e3c4d1f397e84c5825028e78d91fea392
Jessica Owens <jessicadanaowens@gmail.com>
2014-07-28 16:01:01 -0600

    on joins

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -28,5 +28,56 @@ require "pp"
 
 # Customer.all.each {|cust| p cust.name + "," + cust.email}
 
+# pp Customer.select(:id, :name, :email)
 
+# pp Customer.all.length
 
+# puts Order.sum(:amount)
+
+# puts Order.where(
+#   :customer_id=>1
+# ).sum(:amount)
+
+# puts Order.average(:amount)
+
+# puts Order.average(:amount).round(2)
+
+# puts Order.minimum(:amount)
+
+# puts Order.maximum(:amount)
+
+# Order.select(:amount, :customer_id).group(:customer_id).minimum(:amount).each do
+# |key, val| puts key.to_s + ": " + val.to_s
+# end
+
+# Order.select(:amount, :customer_id).group(:customer_id).maximum(:amount).each do |k, v|
+#   puts k.to_s + ": " + v.to_s
+# end
+
+# puts Customer.where(
+#   :state=>"Colorado"
+#   )
+
+# puts Customer.where(
+#   :state=>"Colorado",
+#   :city=>"Rigobertoside"
+#      )
+
+# puts Customer.where("state ='Ohio' or state='Virginia'")
+
+# puts Item.where(:description => "snow board")[0].update(:name => "board01")
+
+# Item.create(
+#   :name => "kayak01",
+#   :description => "one person river kayak"
+# )
+
+# Item.last.destroy
+
+# puts Order.select(:customer_id, :amount).group(:customer_id).sum(:amount)
+
+# Order.joins('INNER JOIN customers ON customers.id = orders.customer_id').select(:id, :amount, :name).each do |user|
+#   puts user.id.to_s + ", " + user.amount.to_s + ", " + user.name
+# end
+
+puts Customer.joins(:orders).select('customers.id as cust_id', :name).group(:name).group(:cust_id).order(:name => :asc).sum(:amount).to_sql
